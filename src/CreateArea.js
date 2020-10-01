524ffed440c5114c286287614af462dcd205fe4a
yk2217 <yahiyakhan2217@gmail.com>
2020-10-01 17:13:29 +0530

    added material ui dependency

--- src/CreateArea.js
+++ src/CreateArea.js
@@ -1,35 +1,42 @@
 import React, { useState } from "react";
-
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 function CreateArea({ submit }) {
   const [title, SetTitle] = useState("");
   const [note, SetNote] = useState("");
+  const [isExpanded, setIsExpanded]=useState(false)
 
   function handleSubmit(e) {
     return submit(title, note, e, SetTitle, SetNote);
   }
 
+  function expanded() {
+    setIsExpanded(true)
+  }
   return (
-    <div style={{padding: "10px", width:"100%"}}>
+    <div style={{ padding: "10px", width: "100%" }}>
       <form>
-        <input
-          name="title"
-          onChange={(e) => SetTitle(e.target.value)}
-          placeholder="Title"
-          value={title}
-        />
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={(e) => SetTitle(e.target.value)}
+            placeholder="Title"
+            value={title}
+          />
+        )}
         <textarea
           value={note}
           onChange={(e) => SetNote(e.target.value)}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded? "3": "1"}
+          onClick={expanded}
         />
         <button
           type="submit"
           disabled={(title.length && note.length) < 1}
           onClick={handleSubmit}
         >
-          Add
+          <AddCircleIcon className="icon" />
         </button>
       </form>
     </div>
--- src/Note.js
+++ src/Note.js
@@ -1,11 +1,12 @@
 import React from "react";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
 function Note({ title, content, dataDelete, id }) {
   return (
     <div className="note">
       <h1>{title}</h1>
       <p>{content}</p>
-      <button onClick={() => dataDelete(id)}>DELETE</button>
+      <button onClick={() => dataDelete(id)}><DeleteForeverIcon className="icon" /></button>
     </div>
   );
 }
